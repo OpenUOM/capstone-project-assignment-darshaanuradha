cacf86984740a4e8dc380df33ca9ce8785e7c3fc
Darsha Anuradha <122999771+darshaanuradha@users.noreply.github.com>
2024-01-08 14:38:18 +0530

    Frontend Tasks

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,7 +51,7 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search1(value) {
     let foundItems = [];
     if (value.length <= 0) {
       this.getStudentData();
@@ -59,6 +59,19 @@ export class StudentTableComponent implements OnInit {
       let b = this.studentData.filter((student) => {
         if (student[0].name.toLowerCase().indexOf(value) > -1) {
           foundItems.push(student)
+        }
+      });
+      this.studentData = foundItems;
+    }
+  }
+  search(value) {
+    let foundItems = [];
+    if (value.length <= 0) {
+      this.getStudentData();
+    } else {
+      let b = this.studentData.filter((teacher) => {
+        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
+          foundItems.push(teacher)
         }
       });
       this.studentData = foundItems;
